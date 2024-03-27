ebcd3e77c7f8526cf6dc07c61c0d7588ffbbab38
MashaSeripaeva <nadyb@DESKTOP-RGIBHB>
2024-03-27 21:20:05 +0300

    cccc

---
+++ Project/Function1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    static class Function1
+    {
+        public static void main()
+        {
+            int a = 5;
+            int b= 6;
+            gg( a,b);
+        }
+        static void gg(  params int[] arr)
+        {
+            if (arr[0]>0)
+            foreach (var item in arr)
+            {
+                item.Print();
+            }
+        }
+    }
+} 
